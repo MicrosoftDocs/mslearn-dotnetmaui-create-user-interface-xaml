c7e4a61a44b968b013563408243485b498884ea9
James Montemagno <james.montemagno@gmail.com>
2022-05-12 14:11:02 -0700

    Add start of exercise2

---
+++ exercise2/Notes/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        if (File.Exists(_fileName))
+        {
+            editor.Text = File.ReadAllText(_fileName);
+        }
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editor.Text = string.Empty;
+    }
+}
+
